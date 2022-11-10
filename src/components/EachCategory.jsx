a5395d29aee3c22b15ea8406011d8fd8409a2e10
Kevin Zheng <kevzheng27@gmail.com>
2022-11-09 20:01:46 -0500

    Build out layout with jsx for EachCategory component

---
+++ src/components/EachCategory.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ListGroup, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+
+function EachCategory({ category, onDeleteCategory }) {
+
+
+
+
+    return (
+        <ListGroup.Item variant="success">
+       
+        <Link style={{textDecoration: 'none'}} to={`/${category.category}/${category.id}`} ><h3 className="text-center">{category.category}</h3></Link> 
+        <Button variant="danger" className="delete-category" onClick={handleDeleteClick} size='sm'>X</Button>
+    
+    
+        </ListGroup.Item>
+    )
+}
+
+export default EachCategory
